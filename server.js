51a99926193710deeaa54c125d72afacaa4d25eb
DiptiAgarwal2004 <diptiagarwal2004@gmail.com>
2025-04-23 22:51:42 +0530

    url change

--- server.js
+++ server.js
@@ -16,8 +16,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+const allowedOrigins = [
+  "http://localhost:5173", // for local dev
+  "https://chat-3h5lzhfa8-diptiagarwal2004-gmailcoms-projects.vercel.app", 
+  "https://chat-app-eta-eight-49.vercel.app",
+  "https://chat-app-git-main-diptiagarwal2004-gmailcoms-projects.vercel.app",
+  "https://chat-3h5lzhfa8-diptiagarwal2004-gmailcoms-projects.vercel.app/"
+];
+
 app.use(cors({
-  origin:[process.env.ORIGIN], 
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }, 
   methods:["GET", "POST", "PUT", "DELETE"],
   credentials:true,      // to enable cookies
 }));
@@ -27,6 +41,7 @@ app.use("/uploads/files", express.static("uploads/files")); // to serve static f
 
 app.use(cookieParser())
 app.use(express.json());    // to parse incoming requests with JSON payloads
+// app.use("")
 app.use("/api/auth", authRoutes)
 app.use("/api/contacts", contactRoutes);
 app.use("/api/messages", messagesRoutes);
